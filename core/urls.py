2c3a7a5d99a8b12be6cb39fd238ad24e3b212a54
unknown <Cetecom@SJAPCA03LC0619.duoc.acad>
2024-11-18 10:58:53 -0300

    changes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import base
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', base, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request,'core/index.html')
+def base(request):
+    return render(request,'core/base.html')
